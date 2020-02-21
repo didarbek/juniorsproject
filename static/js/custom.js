0c23d1c788b4dac9442208831a9b06568ad17196
khaldon <mohamed.khaled33388@gmail.com>
2020-02-21 02:30:12 +0200

    create custom js

---
+++ static/js/custom.js
@@ -0,0 +1,15 @@
+// $.ajax({
+//     url:'http://127.0.0.1:8000/?h=mohamed',
+//     type:'get',
+//     data:{
+//         data:'sldkfj'
+//     },
+//     sucess: function(data){
+//         alert("s");
+//         alert(data);
+        
+//     },
+//     failure: function(data){
+//         alert("errors");
+//     } 
+// })

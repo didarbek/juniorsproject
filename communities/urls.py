cd652f5a5082f9708a4e9e29878318b8ec84d187
khaldon <mohamed.khaled33388@gmail.com>
2020-02-23 17:03:17 +0200

    update

--- communities/urls.py
+++ communities/urls.py
@@ -1,6 +1,24 @@
-from django.urls import path
-from .views import UserSubscriptionListPage
+from django.urls import re_path
+from .views import UserSubscriptionListView, CommunitiesPageView, edit_community_cover, subscribe, new_community, UserCreatedCommunitiesPageView
+app_name = 'communities'
+
 urlpatterns = [
-    path('sub-user/', UserSubscriptionListPage.as_view(), name='sub_user'),
+    re_path(r'^u/(?P<username>[\w.@+-]+)/subscriptions/$',UserSubscriptionListView.as_view(),name='user_subscription_list'),
+    # re_path(r'^trending/$',posts_views.TrendingPageView.as_view(), name='trending'),
+    # re_path(r'^s/(?P<community>[-\w]+)/(?P<post>[-\w]+)/$',posts_views.post_detail,name='post_detail'),
+    # re_path(r'^b/$',CommunitiesPageView.as_view(), name='view_all_communities'),
+    # re_path(r'^b/(?P<community>[-\w]+)/$',communities_views.CommunityPageView.as_view(),name='community'),
+    # re_path(r'^b/ban_user/(?P<community>[-\w]+)/(?P<user_id>\d+)/$',ban_user,name='ban_user'),
+    # re_path(r'^b/(?P<community>[-\w]+)/edit_community_cover/$',edit_community_cover,name='edit_community_cover'),
+    # re_path(r'^b/(?P<community>[-\w]+)/subscription/$',subscribe,name='subscribe'),
+    # re_path(r'^b/(?P<post>[-\w]+)/like/$',like_post,name='like'),
+    # re_path(r'^load_new_comments/$',load_new_comments, name='load_new_comments'),
+    # re_path(r'^u/(?P<username>[\w.@+-]+)/communities/$',UserCreatedCommunitiesPageView.as_view(),name='user_created_communities'),
+    # re_path(r'^u/(?P<username>[\w.@+-]+)/communities/$',UserCreatedCommunitiesPageView.as_view(),name='user_created_communities'),
+    # re_path(r'^new_post/$',posts_views.new_post, name='new_post'),
+    # re_path(r'^delete_post/(?P<post>[-\w]+)/$',posts_views.delete_post, name='delete_post'),
+    # re_path(r'^edit_post/(?P<post>[-\w]+)/$', posts_views.edit_post, name='edit_post'),
+    # re_path(r'^delete_comment/(?P<pk>\d+)/$',delete_comment, name='delete_comment'),
+    # re_path(r'^new_community/$',new_community, name='new_community'),
 
 ]

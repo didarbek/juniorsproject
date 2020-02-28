cceadf185a83b5b2943652822dbabddc70b23f7b
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-02-28 16:42:33 +0400

    changes

--- juniorsproject/urls.py
+++ juniorsproject/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('', include('groups.urls')),
     path('', include('notifications.urls')),
-
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

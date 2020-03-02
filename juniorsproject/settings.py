818dc94e2649cfe0db75a5601f1e3dbf620d6294
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-02 16:13:42 +0400

    postgresql connected

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -122,8 +122,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'juniors',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 

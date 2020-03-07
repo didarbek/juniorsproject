8b658765dad22c7b10739ccf6e318eb2855ce9c6
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-07 17:32:14 +0400

    changes

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -124,15 +124,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'juniors',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

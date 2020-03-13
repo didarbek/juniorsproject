fc69a6b0b61510409d6b7ac5860a61a7044e3549
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-13 13:22:42 +0400

    changes

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -115,6 +115,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'groups.context_processors.SubscriptionList',
 
             ],
         },

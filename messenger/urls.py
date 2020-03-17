8f303f9813fa36537696fed472bb150768b85b5e
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-17 13:49:21 +0400

    changes

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'messenger'
 
 urlpatterns = [
+    path('send/',views.send,name='send_message'),
     path('u/<username>/',views.messages,name='messages'),
     path('',views.inbox,name='inbox'),
-    path('send/',views.send,name='send_message'),
     path('delete/',views.delete,name='delete_message'),
     path('check/',views.check,name='check_message'),
     path('load_new_messages/',views.load_new_messages,name='load_new_messages'),

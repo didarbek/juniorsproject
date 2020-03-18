882f6ed93430504433a2ada7d449ee490ac1a1ed
khaldon <mohamed.khaled33388@gmail.com>
2020-03-18 03:06:12 +0200

    update

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -152,7 +152,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 8000)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }

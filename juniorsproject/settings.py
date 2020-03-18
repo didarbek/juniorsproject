78ec3fa9359f8454deca120876d6a5caa432d64c
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-18 18:25:19 +0400

    changes

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -163,8 +163,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'juniorsproject',
+        'USER': 'juniorsuser',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

01db66da92d9d1e68c9d31e4f58941080b4c6e91
khaldon <mohamed.khaled33388@gmail.com>
2020-03-21 03:09:11 +0200

    update

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -156,6 +156,9 @@ DATABASES = {
 }
 
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 
 
@@ -227,7 +230,5 @@ handler400 = 'juniorsproject.views.error_400'
 
 # DEBUG = False
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

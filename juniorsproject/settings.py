2edd701d8b71af8899e38da5582e4bfea0dbef1c
khaldon <mohamed.khaled33388@gmail.com>
2020-03-21 17:00:45 +0200

    update

--- juniorsproject/settings.py
+++ juniorsproject/settings.py
@@ -144,16 +144,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'juniors',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'juniors',
+#         'USER': 'myprojectuser',
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
